c2f499b164e0768ca26b0d4e3ebf33bd7a0945be
intuinno <intuinno@gmail.com>
2018-03-02 22:38:42 -0500

    Added first extension

---
+++ chrome-extension/myscript.js
@@ -0,0 +1,1 @@
+$('img').parent().append('<table><caption>Shellys Daughters</caption>   <tr>     <th scope="col">Name</th>    <th scope="col">Age</th>    <th scope="col">Birthday</th>    </tr>    <tr>     <th scope="row">Jackie</th>     <td>5</td>     <td>April 5</td>     </tr>     <tr>     <th scope="row">Beth</th>     <td>8</td>     <td>January 14</td>     </tr> 12</table>');
